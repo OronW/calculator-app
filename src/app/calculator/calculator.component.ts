1c1aa35411c86fd0f05d1b2c583dedab0aa6c4a0
OronW <oronwerner@gmail.com>
2019-12-02 17:06:06 +0200

    creating first buttons for testing

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import {InterCalculator} from '../interfaces/InterCalculator';
 
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.sass']
 })
-export class CalculatorComponent implements OnInit {
+export class CalculatorComponent implements InterCalculator { // TODO: Check replacing with switch
 
-  constructor() { }
+  buttons: string[] = ['7', '8', '9', '/' +
+                       '4', '5', '6', '*' +
+                       '1', '2', '3', '-' +
+                       '.', '0', '=', '+' ];
 
-  ngOnInit() {
+  add(a: number, b: number): number {
+    return (a + b);
+  }
+
+  div(a: number, b: number): number {
+    return (a / b);
+  }
+
+  mul(a: number, b: number): number {
+    return (a * b);
+  }
+
+  sub(a: number, b: number): number {
+    return (a - b);
   }
 
 }

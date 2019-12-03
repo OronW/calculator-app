ffbe15bbcbe8ae3f8039be0a3da3d708b641b88c
OronW <oronwerner@gmail.com>
2019-12-04 01:12:23 +0200

    Preventing use of multiple operators. Refining some conditions code. Choosing nicer colors.

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import {InterCalculator} from '../interfaces/InterCalculator';
 
+
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.sass']
 })
-export class CalculatorComponent implements InterCalculator { // TODO: Check replacing interface with switch
-
+export class CalculatorComponent implements InterCalculator { // TODO: Check replacing operation function with switch
+  opFlag: boolean = false;
   result: string = '';
   numpadNumbers: string[] = ['7', '8', '9', '/',
                              '4', '5', '6', '*',
@@ -31,9 +32,20 @@ export class CalculatorComponent implements InterCalculator { // TODO: Check rep
   }
 
   addToExpression(value: string) {
-    if (value == 'Clear') {
+
+    if (value === 'Clear') {
       this.result = '';
-    } else if (value == '=') {
+    }
+    else if ((['+', '-', '*', '/'].indexOf(value) >= 0) &&    // refine the code
+             (['+', '-', '*', '/'].indexOf(this.result[this.result.length - 1]) >= 0)) {
+      this.result = this.result.slice(0, -1);
+      this.result += value;
+
+      // else if (((value === '+') || (value === '-') || (value === '*') || (value === '/')) &&
+      // ((this.result[this.result.length - 1] === '+') || (this.result[this.result.length - 1] === '-') ||
+      // (this.result[this.result.length - 1] === '*') || (this.result[this.result.length - 1] === '/'))) {
+    }
+    else if (value === '=') {
         if (this.result.includes('+', 0)) {
           let splitted = this.result.split('+');
           this.result = this.add(Number(splitted[0]), Number(splitted[1])).toString();
@@ -50,6 +62,7 @@ export class CalculatorComponent implements InterCalculator { // TODO: Check rep
           let splitted = this.result.split('/');
           this.result = this.div(Number(splitted[0]), Number(splitted[1])).toString();
         }
+        this.opFlag = false;
     } else {
         this.result += value;
     }

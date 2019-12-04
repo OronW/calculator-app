3906384bb5a36135f782df9a8695a889ecfa4905
OronW <oronwerner@gmail.com>
2019-12-04 18:13:53 +0200

    New methods for better coding. Added more math rules for calculations. You can now automatically continue calculation after a result or start a new one, depending on your action. Impossible expressions were blocked

--- src/app/calculator/calculator.component.ts
+++ src/app/calculator/calculator.component.ts
@@ -1,82 +1,97 @@
 import { Component } from '@angular/core';
 import {InterCalculator} from '../interfaces/InterCalculator';
 
-
 @Component({
   selector: 'app-calculator',
   templateUrl: './calculator.component.html',
   styleUrls: ['./calculator.component.sass']
 })
-export class CalculatorComponent implements InterCalculator { // TODO: Check replacing operation function with switch
-  opFlag: boolean = false;
-  startFlag: boolean = false;
+export class CalculatorComponent implements InterCalculator {
+  opFlag = false;
+  startFlag = false;
+  equalFlag = false;
+
   result: string = '';
   numpadNumbers: string[] = ['7', '8', '9', '/',
                              '4', '5', '6', '*',
                              '1', '2', '3', '-',
                              '.', '0', '=', '+' ];
   clearBtn = ['Clear'];
-  add(a: number, b: number): number {
-    return (a + b);
+  add(a: number, b: number): number { return (a + b); }
+  div(a: number, b: number): number { return (a / b); }
+  mul(a: number, b: number): number { return (a * b); }
+  sub(a: number, b: number): number { return (a - b); }
+
+  checkOpInString(str: string): number {            // check if a string contains an operation
+    return (['+', '-', '*', '/'].indexOf(str) >= 0 ) ? 1 : 0;
   }
 
-  div(a: number, b: number): number {
-    return (a / b);
+  forceOneOperationOnly(value: string): boolean {  // allow only one operation at a time
+    return ((value === '=') || (this.checkOpInString(value) && (this.opFlag === true)));
   }
 
-  mul(a: number, b: number): number {
-    return (a * b);
+  checkMultipleOperations(value: string): number {  // don't allow multiple operators.
+    return (this.checkOpInString(value) && (this.result.length > 0) && (this.checkOpInString(this.result[this.result.length - 1]))) ;
   }
 
-  sub(a: number, b: number): number {
-    return (a - b);
+  checkIfLegal(value: string): boolean {            // don't start with operation
+    return (this.checkOpInString(value) && ((this.result.length === 0) || (this.startFlag === true)));
   }
 
-  checkOpInString(str: string): number {   // check if a string contains an operation
-    if (['+', '-', '*', '/'].indexOf(str) >= 0 ) {
-      return 1;
+  checkNewExpression(value: string): boolean {      // check if a new expression had started
+   return ((this.equalFlag === true) && (value !== '=') && !(this.checkOpInString(value))) ;
+  }
+
+  calculateExpression(expression: string): string { // split the input string for calculation
+    if (expression.includes('+', 0)) {
+      const splitted = this.result.split('+');
+      return (this.add(Number(splitted[0]), Number(splitted[1])).toString());
+    } else if (expression.includes('-', 0)) {
+        const splitted = this.result.split('-');
+        return (this.sub(Number(splitted[0]), Number(splitted[1])).toString());
+    } else if (expression.includes('*', 0)) {
+        const splitted = this.result.split('*');
+        return (this.mul(Number(splitted[0]), Number(splitted[1])).toString());
+    } else if (expression.includes('/', 0)) {
+        const splitted = this.result.split('/');
+        return (this.div(Number(splitted[0]), Number(splitted[1])).toString());
     }
   }
 
-  addToExpression(value: string) {
+  addToExpression(value: string) {    // build the input string according to the rules
     if (value === 'Clear') {
       this.result = '';
     }
-    else if (this.checkOpInString(value) && this.checkOpInString(this.result[this.result.length - 1])) { // don't allow multiple operators. refined code
-      this.result = this.result.slice(0, -1);   // use only the last operator
+
+    else if (this.checkMultipleOperations(value)) {   // don't allow multiple operators. refined code
+      this.result = this.result.slice(0, -1);         // use only the last operator
       this.result += value;
     }
-    else if ((value === '=') || (this.checkOpInString(value) && (this.opFlag === true))) { // allow only one operation at a time
-      if (this.result.includes('+', 0)) {
-        let splitted = this.result.split('+');
-        this.result = this.add(Number(splitted[0]), Number(splitted[1])).toString();
+
+    else if (this.forceOneOperationOnly(value))  {          // allow only one operation at a time
+      this.result = this.calculateExpression(this.result);  // return the result of the operation
+      if (value === '=') {                                  // if value was "=", reset operation flag
+        this.opFlag = false;
+        this.equalFlag = true;
       }
-      else if (this.result.includes('-', 0)) {
-        let splitted = this.result.split('-');
-        this.result = this.sub(Number(splitted[0]), Number(splitted[1])).toString();
-      }
-      else if (this.result.includes('*', 0)) {
-        let splitted = this.result.split('*');
-        this.result = this.mul(Number(splitted[0]), Number(splitted[1])).toString();
-      }
-      else if (this.result.includes('/', 0)) {
-        let splitted = this.result.split('/');
-        this.result = this.div(Number(splitted[0]), Number(splitted[1])).toString();
-      }
-      if (value === '=') { this.opFlag = false; } // if value was "=", reset condition
-      else { this.result += value; }              // else, continue with next operation
+      else { this.result += value; }                        // else, continue with next operation
     }
+
     else {
-      if (this.checkOpInString(value) && ((this.result.length === 0) || (this.startFlag === true))) {
+      if (this.checkIfLegal(value)) {
         this.result = 'Enter a number first';
         this.startFlag = true;
       }
       else {
-        if (this.startFlag === true) {  // reset result ONLY if expression started with an operation
+        if (this.startFlag === true ) {   // if expression started with an operation - reset the string
           this.result = '';
           this.startFlag = false;
         }
+        if (this.checkNewExpression(value)) {   // if started a new expression - reset the string
+          this.result = '';
+        }
         this.result += value;
+        this.equalFlag = false;
       }
       if (this.checkOpInString(this.result[this.result.length - 1])) {
         this.opFlag = true;
